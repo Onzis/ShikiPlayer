db8f6b448e5add8f8b2c579af5dced3cb5812099
Onzis <deltaradune@yandex.ru>
2025-08-04 15:53:29 +0300

    main

---
+++ manifest.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @namespace       https://github.com/Onzis/ShikiPlayer
+// @name            $(NAME)
+// @description     $(DESCRIPTION)
+// @description:ru  $(DESCRIPTION_RU)
+// @author          $(AUTHOR)
+// @homepageURL     $(HOMEPAGE_URL)
+// @version         $(VERSION)
+//
+// @updateURL       
+// @downloadURL     
+//
+// @match https://shikimori.one/*
+//
+// @run-at          document-end
+// ==/UserScript==
+
+$(USERSCRIPT);
