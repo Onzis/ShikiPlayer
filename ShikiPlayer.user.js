3b156a39e3c2006b991bd7af34fcf2f9d08c638f
Onzis <deltaradune@yandex.ru>
2025-08-07 16:29:37 +0300

    Update ShikiPlayer.user.js

--- ShikiPlayer.user.js
+++ ShikiPlayer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name ShikiPlayer
 // @namespace https://github.com/Onzis/ShikiPlayer
-// @version 1.12
+// @version 1.2.1
 // @description Автоматически загружает видеоплеер для просмотра прямо на Shikimori (Kodik и Alloha)
 // @author Onzis
 // @match https://shikimori.one/*
